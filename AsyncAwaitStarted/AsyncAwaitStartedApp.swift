e24d1c27bbf81fd79424e11c81f206e95d13e620
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2024-02-08 23:34:59 +0900

    Initial Commit

---
+++ AsyncAwaitStarted/AsyncAwaitStartedApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncAwaitStartedApp.swift
+//  AsyncAwaitStarted
+//
+//  Created by joe on 2/8/24.
+//
+
+import SwiftUI
+
+@main
+struct AsyncAwaitStartedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
