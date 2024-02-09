18757c0ea4448f8b284c96d1e0bd2e23d0ec683d
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2024-02-09 23:14:16 +0900

    display Dates on the screen using .task modifier

--- AsyncAwaitStarted/ContentView.swift
+++ AsyncAwaitStarted/ContentView.swift
@@ -17,18 +17,45 @@ struct CurrentDate: Decodable, Identifiable {
 }
 
 struct ContentView: View {
+    @State private var currentDates: [CurrentDate] = []
+    
+    private func getDate() async throws -> CurrentDate? {
+        guard let url = URL(string: "https://ember-sparkly-rule.glitch.me/current-date") else {
+            fatalError("Url is incorrect!")
+        }
+        
+        let (data, _) = try await URLSession.shared.data(from: url)
+        return try? JSONDecoder().decode(CurrentDate.self, from: data)
+    }
+    
+    private func populateDates() async {
+        do {
+            guard let currentDate = try await getDate() else { return }
+            self.currentDates.append(currentDate)
+        } catch {
+            print(error)
+        }
+    }
+    
     var body: some View {
         NavigationView {
-            List(1...10, id: \.self) { index in
-                Text("\(index)")
+            List(currentDates) { currentDate in
+                Text(currentDate.date)
             }.listStyle(.plain)
             
             .navigationTitle("Dates")
-            .navigationBarItems(trailing: Button(action: {
-                // button action
-            }, label: {
-                Image(systemName: "arrow.clockwise.circle")
-            }))
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button(action: {
+                        
+                    }, label: {
+                        Image(systemName: "arrow.clockwise.circle")
+                    })
+                }
+            }
+            .task {
+                await populateDates()
+            }
         }
     }
 }
